e4746e09b4160eb50fabadbd46b29bafc7240a2f
Sumith <sumith@thinkval.com>
2024-12-19 14:58:01 +0530

    Initial Commit

---
+++ src/component/Cards/EventCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './EventCard.css';
+
+const EventCard = ({userName,desc}) => {
+    return (
+        <div className='event-card'>
+            <div className='event-card-image'>
+                <img src={"https://eventox.co/_next/image?url=https%3A%2F%2Fs3.amazonaws.com%2Feventox.co%2Feventox-live%2Fevent%2F_roxw2p6ej-2024-12-12.jpeg&w=640&q=75"} alt='event-image' />
+            </div>
+            <h1>{userName}</h1>
+            <p>{desc}</p>
+        </div>
+    )
+}
+
+export default EventCard;
