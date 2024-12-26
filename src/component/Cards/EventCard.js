8eff19beb3bb4c8c758704cd847e7aa5ed35c761
Sumith <sumith@thinkval.com>
2024-12-26 16:50:22 +0530

    Implement Map in events page

--- src/component/Cards/EventCard.js
+++ src/component/Cards/EventCard.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './EventCard.css';
 
-const EventCard = ({userName,desc}) => {
+const EventCard = (props) => {
+    const {name,date,location,description} = props.data;
     return (
         <div className='event-card'>
             <div className='event-card-image'>
                 <img src={"https://eventox.co/_next/image?url=https%3A%2F%2Fs3.amazonaws.com%2Feventox.co%2Feventox-live%2Fevent%2F_roxw2p6ej-2024-12-12.jpeg&w=640&q=75"} alt='event-image' />
             </div>
-            <h1>{userName}</h1>
-            <p>{desc}</p>
-            <p>Test</p>
-            <p>Test</p>
+            <h1>{name}</h1>
+            <p>{description}</p>
+            <p>{location}</p>
+            <p>{date}</p>
         </div>
     )
 }
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,10 +1,56 @@
 import React from 'react';
+import EventCard from '../component/Cards/EventCard';
 
 const Events = () => {
+    const events = [
+        {
+            name: "Event 1",
+            date: "2024-01-01",
+            location: "New York, NY",
+            description: "This is a description of the event."
+        },
+        {
+            name: "Event 2",
+            date: "2024-01-02",
+            location: "Los Angeles, CA",
+            description: "This is a description of the event."
+        },
+        {
+            name: "Event 3",
+            date: "2024-01-03",
+            location: "Chicago, IL",
+            description: "This is a description of the event."
+        },
+        {
+            name: "Event 4",
+            date: "2024-01-04",
+            location: "San Francisco, CA",
+            description: "This is a description of the event."
+        },
+        {
+            name: "Event 5",
+            date: "2024-01-05",
+            location: "San Francisco, CA",
+            description: "This is a description of the event."
+        }
+    ]
     return (
         <div>
             <h1>Events</h1>
             <p>Here are the events that are happening in the city.</p>
+            <div style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'space-between',
+                gap: '20px'
+            }}>
+
+            
+            {events.map((event,index) => (
+                <EventCard data={event} key={index} />
+            ))}
+            </div>
+
         </div>
     )
 }
