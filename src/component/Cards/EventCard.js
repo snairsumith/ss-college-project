5df58d0a647c8f260cb77d086c43239deab87ecb
Sumith <sumith@thinkval.com>
2024-12-26 16:56:48 +0530

    Image Added

--- src/component/Cards/EventCard.js
+++ src/component/Cards/EventCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './EventCard.css';
 
 const EventCard = (props) => {
-    const {name,date,location,description} = props.data;
+    const {name,date,location,description,image} = props.data;
     return (
         <div className='event-card'>
             <div className='event-card-image'>
-                <img src={"https://eventox.co/_next/image?url=https%3A%2F%2Fs3.amazonaws.com%2Feventox.co%2Feventox-live%2Fevent%2F_roxw2p6ej-2024-12-12.jpeg&w=640&q=75"} alt='event-image' />
+                <img src={image} alt='event-image' />
             </div>
             <h1>{name}</h1>
             <p>{description}</p>
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -7,31 +7,36 @@ const Events = () => {
             name: "Event 1",
             date: "2024-01-01",
             location: "New York, NY",
-            description: "This is a description of the event."
+            description: "This is a description of the event.",
+            image: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/m/m/e/-original-imahfpwvvyctag2h.jpeg?q=70"
         },
         {
             name: "Event 2",
             date: "2024-01-02",
             location: "Los Angeles, CA",
-            description: "This is a description of the event."
+            description: "This is a description of the event.",
+            image: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/z/2/z/-original-imahfpwv6yzawcuc.jpeg?q=70"
         },
         {
             name: "Event 3",
             date: "2024-01-03",
             location: "Chicago, IL",
-            description: "This is a description of the event."
+            description: "This is a description of the event.",
+            image: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/m/m/e/-original-imahfpwvvyctag2h.jpeg?q=70"
         },
         {
             name: "Event 4",
             date: "2024-01-04",
             location: "San Francisco, CA",
-            description: "This is a description of the event."
+            description: "This is a description of the event.",
+            image: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/m/m/e/-original-imahfpwvvyctag2h.jpeg?q=70"
         },
         {
             name: "Event 5",
             date: "2024-01-05",
             location: "San Francisco, CA",
-            description: "This is a description of the event."
+            description: "This is a description of the event.",
+            image: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/m/m/e/-original-imahfpwvvyctag2h.jpeg?q=70"
         }
     ]
     return (
